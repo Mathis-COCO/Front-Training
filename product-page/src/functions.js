6054f0ed3d4c985b0c0075630d0a22cd7da539b2
Mathis-COCO <mathis.coco@ynov.com>
2022-11-02 01:24:05 +0100

    fully working cart with: add/delete options,  empty cart message, cart size, product details in cart

--- product-page/src/functions.js
+++ product-page/src/functions.js
@@ -1,16 +1,27 @@
 const incrementCount = document.getElementById("increment-count")
 const decrementCount = document.getElementById("decrement-count")
 const incrementedCart = document.getElementById("add-to-cart")
+const showCartContent = document.getElementById("cart")
+const clearCartContent = document.getElementById("clear-cart")
 
 let totalCart = document.getElementById("totalCart");
 let count = 0;
 totalCart.innerHTML = count;
 let itemcount = 0;
 let itemsprice = 0;
-let itemcountfull = "aaa";
+let itemcountfull = "Your cart is empty.";
+let itemname = "";
 
-let testcartsize = document.getElementById("test-cart-size");
-testcartsize.innerHTML = itemcountfull;
+let cartcount = document.getElementById("item-count-cart");
+cartcount.innerHTML = itemcount;
+let cartitemname = document.getElementById("item-name-cart");
+cartitemname.innerHTML = itemname;
+let cartfullcontent = document.getElementById("item-price-quantity");
+cartfullcontent.innerHTML = itemcountfull;
+
+let clearcart = document.getElementById("clear-cart");
+let checkoutbutton = document.getElementById("checkout-button");
+
 
 const buttonIncrement = () => {
     count++;
@@ -26,10 +37,32 @@ const buttonDecrement = () => {
 const incrementCart = () => {
     itemcount += count;
     itemsprice += count*parseInt(document.getElementById("item-price").innerHTML);
-    itemcountfull = `${document.getElementById("item-name").innerHTML} ($${document.getElementById("item-price").innerHTML}) x ${itemcount} _______________ cart price : $${itemsprice}`;
-    testcartsize.innerHTML = itemcountfull;
+    itemname = document.getElementById("item-name").innerHTML;
+    itemcountfull = `$${document.getElementById("item-price").innerHTML} x ${itemcount} $${itemsprice}`;
+    cartcount.innerHTML = itemcount;
+    cartfullcontent.innerHTML = itemcountfull;
+    cartitemname.innerHTML = itemname;
+    clearcart = document.getElementById("clear-cart").style.display = "block";
+    checkoutbutton = document.getElementById("checkout-button").style.display = "block";
 };
+
+const showCart = () => {
+    $(".shopping-cart").fadeToggle( "fast");
+}
+
+const clearCart = () => {
+    itemcount = 0;
+    itemname = "";
+    itemcountfull = "Your cart is empty.";
+    cartfullcontent.innerHTML = itemcountfull;
+    cartitemname.innerHTML = itemname;
+    cartcount.innerHTML = itemcount;
+    clearcart = document.getElementById("clear-cart").style.display = "none";
+    checkoutbutton = document.getElementById("checkout-button").style.display = "none";
+}
 
 incrementCount.addEventListener("click", buttonIncrement);
 decrementCount.addEventListener("click", buttonDecrement);
 incrementedCart.addEventListener("click", incrementCart);
+showCartContent.addEventListener("click", showCart);
+clearCartContent.addEventListener("click", clearCart);
